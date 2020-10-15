57db6f4f20b1189f787083b467c01bc35e3f2267
Fdiskbrain(FB) <46346061+fdiskbrain@users.noreply.github.com>
2020-10-15 16:04:29 +0800

    add init default config files

--- tinyproxy/tinyproxy.sh
+++ tinyproxy/tinyproxy.sh
@@ -1,5 +1,35 @@
 #/usr/bin/env bash
 nameProxy="myproxy"
+init(){
+    mkdir -p ~/data/app/tinyproxy
+    [ -f ~/data/app/tinyproxy/tinyproxy.conf ] && cp ~/data/app/tinyproxy/tinyproxy.conf ~/data/app/tinyproxy/tinyproxy.conf.$$.bak
+    cat >~/data/app/tinyproxy/tinyproxy.conf<<EOF
+User tinyproxy
+Group tinyproxy
+
+Port 8888
+Timeout 600
+DefaultErrorFile "/usr/share/tinyproxy/default.html"
+StatFile "/usr/share/tinyproxy/stats.html"
+LogFile "/etc/tinyproxy/tinyproxy.log"
+LogLevel Info
+MaxClients 100
+
+MinSpareServers 5
+MaxSpareServers 20
+
+StartServers 10
+
+MaxRequestsPerChild 0
+ViaProxyName "tinyproxy"
+## upstream http  127.0.0.1:80 ".paypal.com"
+upstream none "127.0.0.1/32"
+upstream none "192.168.0.0/16"
+upstream none "10.0.0.0/8"
+upstream none "172.16.0.0/12"
+upstream none "."
+EOF
+}
 create(){
     docker run --name ${nameProxy}  -d -p 8888:8888 -v ~/data/app/tinyproxy/:/etc/tinyproxy/ fdiskbrain/tinyproxy
 }
@@ -10,6 +40,7 @@ stop(){
     docker stop ${nameProxy}
 }
 start(){
+    [ -f ~/data/app/tinyproxy/tinyproxy.conf ] || init
     docker start ${nameProxy} || create && echo start success
 }
 restart(){
@@ -19,7 +50,10 @@ status(){
     docker inspect ${nameProxy}
 }
 usage(){
-    echo $0 create remove start stop restart
+    echo $0
+    cat <<EOF
+    create remove start stop restart
+EOF
     exit 1
 }
 if [ -z $1 ]; then
